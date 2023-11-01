cbffb2f9519a67b5766b88b33a138b86da65bd33
LukeHalasy <lhalasy@gmail.com>
2023-10-31 21:44:41 -0400

    adds railroad routes

--- client-map/src/app.rs
+++ client-map/src/app.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::time::Duration;
 
 use leptos::*;
@@ -9,21 +10,25 @@ pub fn App() -> impl IntoView {
     let cities = main_city::City::cities();
     let sub_cities = sub_city::SubCity::sub_cities();
 
-    create_effect(move |_| {
-        set_interval_with_handle(
-            move || {
-                set_marker_position.update(|pos| {
-                    pos.lat += 0.001;
-                    pos.lng += 0.001;
-                });
-            },
-            Duration::from_millis(200),
-        )
-        .ok()
-    });
+    let mut rs: HashMap<&deed::Deed, String> = HashMap::new();
+    for de in deed::Deed::rails() {
+        // let color = RandomColor::new()
+        //     .luminosity(random_color::Luminosity::Dark)
+        //     .to_hex();
+        let color = match de {
+            rail_road::Deed::B_AND_M => "#2C3073",
+            rail_road::Deed::B_AND_O => "#0077B0",
+            rail_road::Deed::NYC => "#295536",
+            rail_road::Deed::NYNH_AND_H => "#662D2D",
+            rail_road::Deed::PA => "#A12D2A",
+            _ => "#ff007f",
+        };
+        rs.insert(de, color.into());
+    }
+
+    let d = rail_road::Deed::get_edges();
 
     view! {
-
         <MapContainer style="top:0;left:0;height:100vh;width:100vh,position:absolute" center=Position::new(38.8951100, -77.0363700) zoom=5.0 set_view=true>
             // TODO: need to add attribution
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
@@ -63,28 +68,33 @@ pub fn App() -> impl IntoView {
                     }
                 }).collect_view()
             }
-          //   <Marker position=marker_position >
-          //       <Popup>
-          //           <strong>{"A pretty CSS3 popup"}</strong>
-          //       </Popup>
-          //   </Marker>
-          //     <Marker position=position!(51.5, -0.065) draggable=true >
-          //       <Popup>
-          //           <strong>{"A pretty CSS3 popup"}</strong>
-          //       </Popup>
-          //   </Marker>
-          //   <Tooltip position=position!(51.5, -0.06) permanent=true direction="top">
-          //       <strong>{"And a tooltip"}</strong>
-          //   </Tooltip>
-          //   <Polyline positions=positions(&[(51.505, -0.09), (51.51, -0.1), (51.51, -0.12)])/>
-          //   <Polygon color="purple" positions=positions(&[ (51.515, -0.09), (51.52, -0.1), (51.52, -0.12)]) >
-          //     <Tooltip sticky=true direction="top">
-          //         <strong>{"I'm a polygon"}</strong>
-          //     </Tooltip>
-          // </Polygon>
-          // <Circle center=position!(51.505, -0.09) color="blue" radius=200.0>
-          //     <Tooltip sticky=true>{"I'm a circle"}</Tooltip>
-          // </Circle>
+            {
+                d
+                .into_iter()
+                .map(|n| {
+                    n.1.into_iter().map(|h| {
+                        let c0_coors = (
+                            n.0.0.coordinates().latitude(),
+                            n.0.0.coordinates().longitude(),
+                        );
+                        let c1_coors = (
+                            n.0.1.coordinates().latitude(),
+                            n.0.1.coordinates().longitude(),
+                        );
+
+                        let lat_0 = c0_coors.0;
+                        let lon_0 = c0_coors.1;
+                        let lat_1 = c1_coors.0;
+                        let lon_1 = c1_coors.1;
+
+                        let color: String = rs.get(&h).unwrap().into();
+
+                        view! {
+                            <Polyline color=color positions=positions(&[(lat_0, lon_0), (lat_1, lon_1)]) />
+                        }
+                    }).collect_view()
+                }).collect_view();
+            }
         </MapContainer>
     }
 }
