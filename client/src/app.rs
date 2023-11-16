a61e9d4e61ab234d71ed2986b7eef08f29b61d4c
LukeHalasy <lhalasy@gmail.com>
2023-11-15 20:32:15 -0500

    Basic join game screen. Can send in a PlayerJoined event with input for id, name, and color

--- client/src/app.rs
+++ client/src/app.rs
@@ -1,9 +1,14 @@
 use futures::{SinkExt, StreamExt};
 use reqwasm::websocket::{futures::WebSocket, Message};
+use futures::channel::mpsc::Sender;
 
-use leptos::*;
+use leptos::{*, html::{Input, Select}};
 use leptos_leaflet::*;
-use store::{main_city, Event};
+use leptos_router::{Router, Routes, Route};
+use store::{main_city, Event, Piece, PlayerId};
+use web_sys::SubmitEvent;
+
+use strum::IntoEnumIterator;
 
 use crate::{cities::Cities, rails::Rails};
 
@@ -32,7 +37,73 @@ pub fn App() -> impl IntoView {
         }
     });
 
-    view! {
+    let (name, set_name) = create_signal("".to_string());
+    let (player_id, set_player_id) = create_signal(PlayerId::default());
+
+    // we'll use a NodeRef to store a reference to the input element
+    // this will be filled when the element is created
+    let player_id_input: NodeRef<Input> = create_node_ref();
+    let name_input: NodeRef<Input> = create_node_ref();
+    let color_input: NodeRef<Select> = create_node_ref();
+    
+    // fires when the form `submit` event happens
+    // this will store the value of the <input> in our signal
+    let tx = use_context::<Sender<Event>>().expect("Expected the tx sender");
+    let join_game = move |ev: SubmitEvent| {
+        // stop the page from reloading!
+        ev.prevent_default();
+        
+        set_player_id(player_id_input()
+            .expect("id <input> to exist")
+            .value()
+            .parse::<u64>()
+            .expect("Failed to parse player ID")
+        );
+
+        set_name(name_input()
+            .expect("name <input> to exist")
+            .value());
+
+
+        let color = color_input()
+            .expect("<select> to exist")
+            .value();
+
+        // send Event::PlayerJoined { player_id: 64, name: value, color: Piece::Red } to the server
+        let _ = tx
+            .clone()
+            .try_send(Event::PlayerJoined {
+                player_id: player_id.clone().get(),
+                name: name.clone().get(),
+                color: Piece::from_str(&color).unwrap(),
+            });
+        
+        let navigate = leptos_router::use_navigate();
+        navigate("/map", Default::default());
+    };
+    
+    let home_page = view! {
+        <div>
+            <h1>"Welcome to the game!"</h1>
+            <form on:submit=join_game>
+                <input type="text" value=player_id node_ref=player_id_input placeholder="Enter Player ID" pattern="[0-9]*"/>
+                <input type="text" value=name node_ref=name_input placeholder="Enter your name"/>
+                <select node_ref=color_input>
+                    {
+                        Piece::iter()
+                        .map(move |p| {
+                            view! {
+                                <option value=p.to_string()>{p.to_string()}</option>
+                            }
+                        }).collect_view()
+                    }
+                </select>
+                <input type="submit" value="Join Game"/>
+            </form>
+        </div>
+    };
+
+    let main_page = view! {
         <MapContainer style="top:0;left:0;height:100vh;width:100vh,position:absolute" center=Position::new(39.8283, -98.5795) zoom=5.0 max_zoom=7.5 min_zoom=5.0 set_view=true>
             // TODO: need to add attribution
             <TileLayer url="https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png"/>
@@ -42,5 +113,14 @@ pub fn App() -> impl IntoView {
 
             <Marker position={player_location}></Marker>
         </MapContainer>
+    };
+
+    view! {
+        <Router>
+            <Routes>
+                <Route path="/" view={move || home_page.clone()} />
+                <Route path="/map" view={move || main_page.clone()} />
+            </Routes>
+        </Router>
     }
 }
--- store/src/lib.rs
+++ store/src/lib.rs
@@ -8,10 +8,11 @@ use rail::Rail;
 use rail::C;
 use region::Region;
 use serde::{Deserialize, Serialize};
+use strum::EnumIter;
 use strum::IntoEnumIterator;
 
 use crate::{rail::RAILROAD_GRAPH, travel_payout::travel_payout};
-type PlayerId = u64;
+pub type PlayerId = u64;
 pub type Cash = u64;
 
 pub mod dice;
@@ -44,7 +45,7 @@ pub enum InGameStage {
 //     MovementRoll,
 // }
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, EnumIter)]
 pub enum Piece {
     Yellow,
     Green,
@@ -52,6 +53,34 @@ pub enum Piece {
     Orange,
     Purple,
     Red,
+}
+
+impl std::fmt::Display for Piece {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Piece::Red => write!(f, "Red"),
+            Piece::Blue => write!(f, "Blue"),
+            Piece::Green => write!(f, "Green"),
+            Piece::Yellow => write!(f, "Yellow"),
+            Piece::Orange => write!(f, "Orange"),
+            Piece::Purple => write!(f, "Purple"),
+        }
+    }
+}
+
+// create a from string method for Piece
+impl Piece {
+    pub fn from_str(s: &str) -> Option<Self> {
+        match s {
+            "Red" => Some(Piece::Red),
+            "Blue" => Some(Piece::Blue),
+            "Green" => Some(Piece::Green),
+            "Yellow" => Some(Piece::Yellow),
+            "Orange" => Some(Piece::Orange),
+            "Purple" => Some(Piece::Purple),
+            _ => None,
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Eq)]
@@ -139,6 +168,11 @@ pub enum Event {
     },
     EndPurchaseStage {
         player_id: PlayerId,
+    },
+    PlayerJoined {
+        player_id: PlayerId,
+        name: String,
+        color: Piece
     },
 }
 
@@ -299,6 +333,25 @@ impl State {
             }
             EndPurchaseStage { .. } => {
                 self.advance_turn();
+            }
+            PlayerJoined { player_id, name, color } => {
+                self.players.insert(
+                    *player_id,
+                    Player {
+                        cash: 0,
+                        name: name.clone(),
+                        piece: color.clone(),
+                        home_city: None,
+                        route_history: vec![],
+                        start: None,
+                        destination: None,
+                        spaces_left_to_move: None,
+                        rails: vec![],
+                        engine: Engine::Freight,
+                    },
+                );
+
+                self.player_order.push(*player_id);
             }
             // TODO: Remove
             _ => {}
@@ -506,6 +559,22 @@ impl State {
 
                 // ensure that it's the purchase stage
                 if self.stage != Stage::InGame(InGameStage::Purchase) {
+                    return false;
+                }
+            }
+            PlayerJoined { player_id, name, color } => {
+                // Check player doesn't already exist
+                if self.players.contains_key(player_id) {
+                    return false;
+                }
+
+                // Check that no other player has the same color
+                if self.players.values().any(|player| player.piece == *color) {
+                    return false;
+                }
+                
+                // Check that the player name is unique
+                if self.players.values().any(|player| player.name == *name) {
                     return false;
                 }
             }
