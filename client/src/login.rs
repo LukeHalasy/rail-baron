832b5d70e422dc6a71e2291f30fbac57d89ad184
Luke Halasy <lhalasy@gmail.com>
2023-11-18 14:07:41 -0500

    Login wallpaper aesthetic (#6)

--- client/src/login.rs
+++ client/src/login.rs
@@ -39,7 +39,6 @@ pub fn Login() -> impl IntoView {
                 piece: Piece::from_str(&piece).unwrap(),
             });
 
-        // TODO: Add error handling in case request fails
         // Update the player information
         set_player_information.update(|player| {
             *player = Some(Player {
@@ -58,23 +57,34 @@ pub fn Login() -> impl IntoView {
     };
     
     view! {
-        <div>
-            <h1>"Welcome to the game!"</h1>
-            <form on:submit=join_game>
-                <input type="text" node_ref=player_id_input placeholder="Enter Player ID" pattern="[0-9]*"/>
-                <input type="text" node_ref=name_input placeholder="Enter your name"/>
-                <select node_ref=piece_input>
-                    {
-                        Piece::iter()
-                        .map(move |p| {
-                            view! {
-                                <option value=p.to_string()>{p.to_string()}</option>
-                            }
-                        }).collect_view()
-                    }
-                </select>
-                <input type="submit" value="Join Game"/>
-            </form>
+        <div style="position: relative; height: 100vh; width: 100vw; display: flex; justify-content: center; align-items: center;">
+            <div style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-image: url('assets/images/rail-riches.png'); background-size: 100% 100%;"></div>
+            <div style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); display: flex; justify-content: center; align-items: center;">
+                <div style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); display: flex; justify-content: center; align-items: flex-start;">
+                    <form on:submit=join_game style="display: flex; flex-direction: column; align-items: center; margin-top: 20px;">
+                        <h1 style="text-align: center; margin-bottom: 20px; font-family: 'OldTimey', sans-serif; font-size: 88px; color: #1A4176; text-decoration: underline dashed; text-shadow: -2px -2px 0 #999999, 2px -2px 0 #999999, -2px 2px 0 #999999, 2px 2px 0 #999999; color: #0000a0;">Railway Riches</h1>
+                        <div style="background-color: rgba(153, 153, 153, 1); padding: 30px;">
+                            // <input type="text" node_ref=player_id_input placeholder="Enter Player ID" pattern="[0-9]*" style="margin-bottom: 10px;"/>
+                            // <input type="text" node_ref=name_input placeholder="Enter your name" style="margin-bottom: 10px;"/>
+                            // <select node_ref=piece_input style="margin-bottom: 10px;">
+                            //     {
+                            //         Piece::iter()
+                            //         .map(move |p| {
+                            //             view! {
+                            //                 <option value=p.to_string()>{p.to_string()}</option>
+                            //             }
+                            //         }).collect_view()
+                            //     }
+                            // </select>
+                            // <input type="submit" value="Join Game"/>
+                            <div style="display: flex; flex-direction: column; align-items: center; justify-content: center;">
+                                <input type="submit" value="Create Game" style="background-color: #1A4176; width: 200px; color: #DDDDDD; font-size: 16px; padding: 10px 20px; margin-bottom: 15px; border: none; cursor: pointer;" />
+                                <input type="submit" value="Join Lobby" style="background-color: #1A4176; width: 200px; color: #DDDDDD; font-size: 16px; padding: 10px 20px;  border: none; cursor: pointer;"/>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     }
 }
