2f57da701d3df32683327a9d15cea3e1cf294b6d
LukeHalasy <lhalasy@gmail.com>
2023-12-26 12:18:28 -0500

    store computer test. Change to run multiple games and quit if player to max is elimenated

--- store/src/main.rs
+++ store/src/main.rs
@@ -1,99 +1,130 @@
-use std::io::Write;
+use std::io::{self, Write};
 
 use store::{Event, Piece, State};
 
 // TODO: Think about a better way to test out the computer
 fn main() {
-    // Initialize a new game
-    let mut game_state = State::default();
+    // set the number of games to test
+    let num_games = 100;
+    let game_vec = vec![0; num_games];
 
-    let setup_events = [
-        Event::Create { player_id: 0 },
-        Event::PlayerJoined { player_id: 1 },
-        Event::PlayerJoined { player_id: 2 },
-        Event::PlayerJoined { player_id: 3 },
-        Event::SetPlayerAttributes {
-            player_id: 0,
-            name: "C1".to_string(),
-            piece: Piece::Red,
-        },
-        Event::SetPlayerAttributes {
-            player_id: 1,
-            name: "C2".to_string(),
-            piece: Piece::Blue,
-        },
-        Event::SetPlayerAttributes {
-            player_id: 2,
-            name: "C3".to_string(),
-            piece: Piece::Green,
-        },
-        Event::SetPlayerAttributes {
-            player_id: 3,
-            name: "C4".to_string(),
-            piece: Piece::Purple,
-        },
-        Event::Start { player_id: 0 },
-        Event::OrderRollRequest { player_id: 0 },
-        Event::OrderRollRequest { player_id: 1 },
-        Event::OrderRollRequest { player_id: 2 },
-        Event::OrderRollRequest { player_id: 3 },
-    ];
+    // run num_games counting the number of times the computer wins
+    let mut computer_wins = 0;
+    for (count, _) in game_vec.iter().enumerate() {
+        println!("STARTING GAME # {}", count);
+        // Initialize a new game
+        let mut game_state = State::default();
 
-    for event in setup_events.iter() {
-        if let Err(err) = game_state.validate(event) {
-            panic!("Invalid event: {:?}", err);
+        let setup_events = [
+            Event::Create { player_id: 0 },
+            Event::PlayerJoined { player_id: 1 },
+            Event::PlayerJoined { player_id: 2 },
+            Event::PlayerJoined { player_id: 3 },
+            Event::SetPlayerAttributes {
+                player_id: 0,
+                name: "C1".to_string(),
+                piece: Piece::Red,
+            },
+            Event::SetPlayerAttributes {
+                player_id: 1,
+                name: "C2".to_string(),
+                piece: Piece::Blue,
+            },
+            Event::SetPlayerAttributes {
+                player_id: 2,
+                name: "C3".to_string(),
+                piece: Piece::Green,
+            },
+            Event::SetPlayerAttributes {
+                player_id: 3,
+                name: "C4".to_string(),
+                piece: Piece::Purple,
+            },
+            Event::Start { player_id: 0 },
+            Event::OrderRollRequest { player_id: 0 },
+            Event::OrderRollRequest { player_id: 1 },
+            Event::OrderRollRequest { player_id: 2 },
+            Event::OrderRollRequest { player_id: 3 },
+        ];
+
+        for event in setup_events.iter() {
+            if let Err(err) = game_state.validate(event) {
+                panic!("Invalid event: {:?}", err);
+            }
+            game_state.consume(event);
         }
-        game_state.consume(event);
-    }
 
-    // Now, let the computer play four rounds
-    println!("Game state: {:#?}", game_state);
+        // Now, let the computer play four rounds
+        let mut i = 0;
+        let player_to_max_id = 2;
+        while game_state.stage != store::Stage::Ended {
+            // println!("Game: {}, Round: {}", count, i);
+            // io::stdout().flush().unwrap();
 
-    let rounds = 500;
-    let mut i = 0;
-    let player_to_max_id = 2;
-    while i < rounds * game_state.players.len() {
-        let active_player = game_state.active_player_id.unwrap();
-        let (_, event) = game_state.minimax(
-            4,
-            player_to_max_id,
-            false,
-            setup_events.last().unwrap().clone(),
-        );
-        // ensure the event is valid
-        match event {
-            Event::HomeRoll { .. } => {}
-            Event::DestinationRoll { .. } => {}
-            Event::OrderRoll { .. } => {}
-            _ => {
-                if let Err(err) = game_state.validate(&event) {
-                    // if an invalid event occurs print out the event history to a json file
-                    let filename = "event_history.json".to_string();
-                    let mut file = std::fs::File::create(filename.clone()).unwrap();
 
-                    let json = serde_json::to_string_pretty(&game_state.history).unwrap();
-                    // wirite the json to the file
-                    println!("Writing event history to file: {}", filename.clone());
-                    file.write_all(json.as_bytes()).unwrap();
+            let active_player = game_state.active_player_id.unwrap();
+            // Thought.. what happens if the player gets eliminated...
+            let (_, event) = game_state.minimax(
+                15,
+                player_to_max_id,
+                false,
+                setup_events.last().unwrap().clone(),
+            );
+            // ensure the event is valid
+            match event {
+                Event::HomeRoll { .. } => {}
+                Event::DestinationRoll { .. } => {}
+                Event::OrderRoll { .. } => {}
+                _ => {
+                    if let Err(err) = game_state.validate(&event) {
+                        // if an invalid event occurs print out e event history to a json file
+                        let filename = "event_history.json".to_string();
+                        let mut file = std::fs::File::create(filename.clone()).unwrap();
 
-                    println!("STATE: {:#?}", game_state);
-                    panic!("Invalid event: {:#?}, {:?}", event, err);
+                        let json = serde_json::to_string_pretty(&game_state.history).unwrap();
+                        // wirite the json to the file
+                        println!("Writing event history to file: {}", filename.clone());
+                        file.write_all(json.as_bytes()).unwrap();
+
+                        // write the game state to a json file
+                        let filename = "state.json".to_string();
+                        let mut file = std::fs::File::create(filename.clone()).unwrap();
+
+                        let json = serde_json::to_string_pretty(&game_state).unwrap();
+                        file.write_all(json.as_bytes()).unwrap();
+
+                        // println!("STATE: {:#?}", game_state);
+                        panic!("Invalid event: {:#?}, {:?}", event, err);
+                    }
                 }
             }
+
+            // println!("Event: {:#?}", event);
+            game_state.consume(&event);
+
+            // check if the player to max id has been eliminted
+            if game_state.player_order.contains(&player_to_max_id) == false {
+                println!("The AI Lost! Player {} has been eliminated!", player_to_max_id);
+
+                break;
+                // find the next player to max id
+                // let mut next_player_to_max_id = player_to_max_id + 1;
+                // while game_state.players.contains_key(&next_player_to_max_id) == false {
+                //     next_player_to_max_id += 1;
+                // }
+                // println!("Next player to max id: {}", next_player_to_max_id);
+                // player_to_max_id = next_player_to_max_id;
+            }
+
+            i += 1;
         }
 
-        println!("Round finished: {}, Player_id: {}", i, active_player);
-        println!("Event: {:#?}", event);
-
-        game_state.consume(&event);
-
-        i += 1;
+        if game_state.winner.is_some() && game_state.winner.unwrap() == player_to_max_id {
+            computer_wins += 1;
+            println!("The computer won!");
+        }
     }
-    println!("Game state: {:#?}", game_state);
 
-    let mut file = std::fs::File::create("state.json").unwrap();
-    let json = serde_json::to_string_pretty(&game_state).unwrap();
-    // wirite the json to the file
-    println!("Writing state to file: state.json");
-    file.write_all(json.as_bytes()).unwrap();
+    println!("The computer won {} out of {} games", computer_wins, game_vec.len());
 }
+
