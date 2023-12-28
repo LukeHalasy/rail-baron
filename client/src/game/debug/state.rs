0df430ddb5fcfc2b74e11571c6ea77913446f08b
LukeHalasy <lhalasy@gmail.com>
2023-12-28 11:48:29 -0500

    adds a state debug menu

--- client/src/game/debug/state.rs
+++ client/src/game/debug/state.rs
@@ -4,20 +4,19 @@ use leptos_router::use_params;
 use crate::{app::PlayerId, pre_game::lobby::LobbyParams};
 
 #[component]
-pub fn EventHistoryDebug() -> impl IntoView {
+pub fn StateDebug() -> impl IntoView {
     let _id = move || {
         use_params::<LobbyParams>()
             .with(|params| params.as_ref().map(|params| params.id).unwrap_or_default())
     };
 
-    let _player_id = use_context::<PlayerId>().expect("Expected a player id signal");
     let game_state =
         use_context::<ReadSignal<Option<store::State>>>().expect("Expected a game state signal");
 
-    let event_history = move || {
+    let game_state_debug = move || {
         if game_state.get().is_some() {
             view! {
-                <code class="language-json">{ serde_json::to_string_pretty(&game_state.get().unwrap().history).unwrap() }</code>
+                <code class="language-json">{ serde_json::to_string_pretty(&game_state.get().unwrap()).unwrap() }</code>
             }
             .into_view()
         } else {
@@ -27,10 +26,10 @@ pub fn EventHistoryDebug() -> impl IntoView {
 
     view! {
         <details>
-            <summary>Event History</summary>
+            <summary>Game State</summary>
             <div style="height:40vh;">
                 <pre class="language-json">
-                    { event_history }
+                    { game_state_debug }
                 </pre>
             </div>
         </details>
