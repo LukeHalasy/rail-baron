7bcc4b0cc2bd745af31c85a9ef9b22ebdc1bb2c8
LukeHalasy <lhalasy@gmail.com>
2023-12-28 11:47:13 -0500

    computer server logic + proper dice roll handling (send dice roll results to client instead of having client re-roll)

--- store/src/lib.rs
+++ store/src/lib.rs
@@ -42,6 +42,7 @@ pub enum ServerMessage {
 pub enum ClientMessage {
     Event(Event),
     JoinGame(GameId),
+    AddComputer(GameId),
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
@@ -334,6 +335,9 @@ pub enum Event {
     PlayerJoined {
         player_id: PlayerId,
     },
+    ComputerJoined {
+        player_id: PlayerId,
+    },
     // Bankrupcty Events
     SellRailToBank {
         player_id: PlayerId,
@@ -357,7 +361,7 @@ pub enum Event {
     ChangeStage {
         stage: Stage,
         line: u32,
-    }
+    },
 }
 
 impl State {
@@ -365,14 +369,37 @@ impl State {
         use Event::*;
 
         match valid_event {
-            Create { player_id } | PlayerJoined { player_id } => {
+            Create { player_id } | PlayerJoined { player_id } | ComputerJoined { player_id } => {
                 if let Create { player_id } = valid_event {
+                    println!("SETTING ACTIVE PLAYER ID");
                     self.game_host = Some(*player_id);
+                    self.active_player_id = Some(*player_id);
                 }
+
+                // TODO: Remove so that names + piece colors are not rng
+                let is_computer = matches!(valid_event, ComputerJoined { .. });
+                let name = if is_computer {
+                    format!("Computer {}", player_id)
+                } else {
+                    format!("Player {}", player_id)
+                };
+
+                // select a piece that hasn't been used yet
+                let piece = Piece::iter()
+                    .find(|piece| {
+                        !self
+                            .players
+                            .iter()
+                            .any(|(_, player)| player.piece == Some(*piece))
+                    })
+                    .expect("Expected there to be a piece available");
 
                 self.players.insert(
                     *player_id,
                     Player {
+                        computer: is_computer,
+                        piece: Some(piece),
+                        name: Some(name),
                         ..Player::default()
                     },
                 );
@@ -610,21 +637,34 @@ impl State {
                 }
             }
             HomeRollRequest { player_id } => {
-                self.history.push(valid_event.clone());
-
                 let (region_roll, region) = DiceRoll::region();
                 let (city_roll, city) = DiceRoll::city_in_region(region);
 
-                self.players.get_mut(player_id).unwrap().home_city = Some(city);
-                self.players.get_mut(player_id).unwrap().start = Some(city);
-
-                self.history.push(Event::HomeRoll {
+                self.consume(&Event::HomeRoll {
                     player_id: *player_id,
                     region_roll,
                     city_roll,
                     region,
                     city,
                 });
+            }
+            HomeRoll {
+                player_id,
+                region_roll,
+                city_roll,
+                region,
+                city,
+            } => {
+                self.history.push(Event::HomeRoll {
+                    player_id: *player_id,
+                    region_roll: *region_roll,
+                    city_roll: *city_roll,
+                    region: *region,
+                    city: *city,
+                });
+
+                self.players.get_mut(player_id).unwrap().home_city = Some(*city);
+                self.players.get_mut(player_id).unwrap().start = Some(*city);
 
                 // if all player's have rolled for their home city then change the stage
                 if self
@@ -644,13 +684,17 @@ impl State {
                 self.advance_turn();
             }
             OrderRollRequest { player_id } => {
-                self.history.push(valid_event.clone());
-
                 let roll = DiceRoll::red_and_white();
 
-                self.history.push(Event::OrderRoll {
+                self.consume(&Event::OrderRoll {
                     player_id: *player_id,
                     roll,
+                });
+            }
+            OrderRoll { player_id, roll } => {
+                self.history.push(Event::OrderRoll {
+                    player_id: *player_id,
+                    roll: *roll,
                 });
 
                 // TODO: Handle Ties
@@ -702,11 +746,12 @@ impl State {
                         line: line!(),
                         stage: Stage::InGame(InGameStage::HomeRoll),
                     });
+                } else {
+                    // move to the next player
+                    self.advance_turn();
                 }
             }
             DestinationRollRequest { player_id } => {
-                self.history.push(valid_event.clone());
-
                 let (region_roll, region) = DiceRoll::region();
                 let mut city_roll;
                 let mut city;
@@ -717,15 +762,36 @@ impl State {
                     city == self.players.get(player_id).unwrap().start.unwrap()
                 } {}
 
-                self.history.push(Event::DestinationRoll {
+                self.consume(&Event::DestinationRoll {
                     player_id: *player_id,
                     region_roll,
                     city_roll,
                     region,
                     city,
                 });
+            }
+            DestinationRoll {
+                player_id,
+                region_roll,
+                city_roll,
+                region,
+                city,
+            } => {
+                self.history.push(Event::DestinationRoll {
+                    player_id: *player_id,
+                    region_roll: *region_roll,
+                    city_roll: *city_roll,
+                    region: *region,
+                    city: *city,
+                });
 
-                if !self.players.get(player_id).unwrap().route_history.is_empty() {
+                if !self
+                    .players
+                    .get(player_id)
+                    .unwrap()
+                    .route_history
+                    .is_empty()
+                {
                     self.consume(&Event::ChangeStage {
                         line: line!(),
                         stage: Stage::InGame(InGameStage::Purchase),
@@ -749,20 +815,25 @@ impl State {
                     self.advance_turn();
                 }
 
-                self.players.get_mut(player_id).unwrap().destination = Some(city);
+                self.players.get_mut(player_id).unwrap().destination = Some(*city);
             }
             MovementRollRequest { player_id } => {
-                self.history.push(valid_event.clone());
-
-                let player: &mut Player = self.players.get_mut(player_id).unwrap();
+                let player = self.players.get(player_id).unwrap();
                 let roll = DiceRoll::movement_roll(&player.engine);
 
-                player.spaces_left_to_move = Some(roll.sum());
-
-                self.history.push(Event::MovementRoll {
+                self.consume(&Event::MovementRoll {
                     player_id: *player_id,
                     roll,
                 });
+            }
+            MovementRoll { player_id, roll } => {
+                self.history.push(Event::MovementRoll {
+                    player_id: *player_id,
+                    roll: *roll,
+                });
+
+                let player = self.players.get_mut(player_id).unwrap();
+                player.spaces_left_to_move = Some(roll.sum());
 
                 self.consume(&Event::ChangeStage {
                     line: line!(),
@@ -974,7 +1045,6 @@ impl State {
             ChangeStage { line: _, stage } => {
                 self.stage = *stage;
             }
-            _ => {}
         }
 
         match valid_event {
@@ -982,6 +1052,28 @@ impl State {
             HomeRollRequest { player_id: _ } => {}
             DestinationRollRequest { player_id: _ } => {}
             MovementRollRequest { player_id: _ } => {}
+            OrderRoll {
+                player_id: _,
+                roll: _,
+            } => {}
+            HomeRoll {
+                player_id: _,
+                region_roll: _,
+                city_roll: _,
+                region: _,
+                city: _,
+            } => {}
+            DestinationRoll {
+                player_id: _,
+                region_roll: _,
+                city_roll: _,
+                region: _,
+                city: _,
+            } => {}
+            MovementRoll {
+                player_id: _,
+                roll: _,
+            } => {}
             _ => self.history.push(valid_event.clone()),
         }
     }
@@ -998,6 +1090,8 @@ impl State {
                 break;
             }
         }
+
+        println!("Active player changed to {:?}", self.active_player_id);
     }
 
     pub fn validate(&self, event: &Event) -> Result<(), String> {
@@ -1201,6 +1295,11 @@ impl State {
                     return Err("It is not the order roll stage".to_string());
                 }
 
+                // check player is currently the one making their move
+                if self.active_player_id != Some(*player_id) {
+                    return Err("It is not this player's turn".to_string());
+                }
+
                 // Check player exists
                 if !self.players.contains_key(player_id) {
                     return Err("Player does not exist".to_string());
@@ -1361,7 +1460,7 @@ impl State {
                     return Err("It is not this player's turn".to_string());
                 }
             }
-            PlayerJoined { player_id } => {
+            PlayerJoined { player_id } | ComputerJoined { player_id } => {
                 // Check player doesn't already exist
                 if self.players.contains_key(player_id) {
                     return Err("Player already exists".to_string());
